6eac10acc4b818cd46856ebc20005bc2a3c49f8c
LostFrover <347531316@qq.com>
2026-09-22 22:47:47 +0800

    修复: 补充 btnEditScript_Click 事件处理方法, 解决编译错误

--- Views/ScriptLoopPage.xaml.cs
+++ Views/ScriptLoopPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Pc_clicker.Views
@@ -10,6 +11,11 @@ namespace Pc_clicker.Views
         public ScriptLoopPage()
         {
             InitializeComponent();
+        }
+
+        private void btnEditScript_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: 通过记事本打开当前选中的脚本文件进行编辑
         }
     }
 }
