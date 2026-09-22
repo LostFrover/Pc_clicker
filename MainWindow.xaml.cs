93f856b05912edc560dc7c6b22d414d68cf1273c
LostFrover <347531316@qq.com>
2026-09-22 22:32:46 +0800

    重构: 将 Qt .ui 界面迁移为 WPF XAML 界面
    - MainWindow.xaml 按 UI.ui 重建: 菜单栏/子页面容器/循环类型选择/执行按钮
    - 新增 Views/SinglePointPage: 对应 page_single.ui 单点循环子界面
    - 新增 Views/ScriptLoopPage: 对应 page_multi.ui 脚本循环子界面
    - MainWindow.xaml.cs 实现页面切换/数字输入限制/循环切换重置
    - csproj 移除已删除的 func 引用, 引入 Views 新文件

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Pc_clicker.Views;
 
 namespace Pc_clicker
 {
@@ -20,22 +10,54 @@ namespace Pc_clicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly SinglePointPage _singlePage = new SinglePointPage();
+        private readonly ScriptLoopPage _scriptPage = new ScriptLoopPage();
+
         public MainWindow()
         {
             InitializeComponent();
+            pageHost.Content = _singlePage;
+            locating.IsEnabled = false;
         }
 
-        private void Button_Recorder(object sender, RoutedEventArgs e)
+        private void MenuPageSingle_Click(object sender, RoutedEventArgs e)
         {
-
+            pageHost.Content = _singlePage;
+            locating.IsEnabled = false;
         }
-        private void Button_Config(object sender, RoutedEventArgs e)
-        {
 
+        private void MenuPageMulti_Click(object sender, RoutedEventArgs e)
+        {
+            pageHost.Content = _scriptPage;
+            locating.IsEnabled = true;
         }
-        private void Button_Excutor(object sender, RoutedEventArgs e)
-        {
 
+        private void MenuPageGuide_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: 打开指导文档
+        }
+
+        private void LoopMode_Checked(object sender, RoutedEventArgs e)
+        {
+            if (loop_num != null)
+            {
+                loop_num.Text = "1";
+            }
+        }
+
+        private void LoopNum_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, "^[0-9]+$");
+        }
+
+        private void Locating_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: 创建跟随鼠标的坐标气泡浮窗
+        }
+
+        private void Excuting_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: 开始/停止执行循环
         }
     }
 }
---
+++ Views/SinglePointPage.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace Pc_clicker.Views
+{
+    /// <summary>
+    /// SinglePointPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class SinglePointPage : UserControl
+    {
+        public SinglePointPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
